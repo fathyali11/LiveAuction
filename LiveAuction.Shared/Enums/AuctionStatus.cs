862ee34fa91b5f28f372a8fd752ad24dfadab073
fathy <fathy@FATHYALI>
2025-12-10 01:45:37 +0200

    create auction status

---
+++ LiveAuction.Shared/Enums/AuctionStatus.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LiveAuction.Shared.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum AuctionStatus
+{
+    Pending=0,
+    Open=1,
+    Closed= 2
+}
