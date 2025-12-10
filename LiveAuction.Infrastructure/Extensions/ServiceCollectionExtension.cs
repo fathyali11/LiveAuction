ab6142c686c5333a4c3a05339c28af4aee8d1a50
fathy <fathy@FATHYALI>
2025-12-10 20:52:14 +0200

    inject services

--- LiveAuction.Infrastructure/Extensions/ServiceCollectionExtension.cs
+++ LiveAuction.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using LiveAuction.Domain.Repositories;
+using LiveAuction.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace LiveAuction.Infrastructure.Extensions;
 public static class ServiceCollectionExtension
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
     {
-        // Add infrastructure services here
-        //services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IAuctionRepository,AuctionRepository>();
+        services.AddScoped<IBidRepository,BidRepository>();
         return services;
     }
 }
