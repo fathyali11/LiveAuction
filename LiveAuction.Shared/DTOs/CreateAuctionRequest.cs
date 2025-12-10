434806cf3564e3702117b7d0bc433d62d43a34e3
fathy <fathy@FATHYALI>
2025-12-10 02:00:07 +0200

    create CreateAuctionRequest

---
+++ LiveAuction.Shared/DTOs/CreateAuctionRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace LiveAuction.Shared.DTOs;
+
+public class CreateAuctionRequest
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
+    public DateTime StartTime { get; set; } = DateTime.UtcNow;
+    public DateTime EndTime { get; set; }
+    public decimal StartingBid { get; set; }
+    public string Seller { get; set; } = string.Empty;
+    public int DurationInMinutes { get; set; }
+
+}
