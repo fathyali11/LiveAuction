a41c1895ef4539ad083c8b1b6eac4522a19babea
fathy <fathy@FATHYALI>
2025-12-10 21:35:26 +0200

    add jwt  settings

--- LiveAuction.Infrastructure/Extensions/ServiceCollectionExtension.cs
+++ LiveAuction.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -1,11 +1,16 @@
-﻿using LiveAuction.Domain.Entities;
+﻿using LiveAuction.Domain.Consts;
+using LiveAuction.Domain.Entities;
 using LiveAuction.Domain.Repositories;
 using LiveAuction.Infrastructure.Presistence;
 using LiveAuction.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace LiveAuction.Infrastructure.Extensions;
 public static class ServiceCollectionExtension
@@ -27,6 +32,35 @@ public static class ServiceCollectionExtension
         })
             .AddEntityFrameworkStores<ApplicationDbContext>()
             .AddDefaultTokenProviders();
+
+        services.AddOptions<JwtSettings>()
+            .Bind(configuration.GetSection(nameof(JwtSettings)))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+
+        var jwtSettings = configuration.GetSection(nameof(JwtSettings)).Get<JwtSettings>();
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings!.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey)),
+                    ClockSkew = TimeSpan.FromMinutes(5)
+                };
+            });
+           
 
 
 
