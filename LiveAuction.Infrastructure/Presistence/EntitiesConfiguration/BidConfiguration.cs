fd257579803df2f16f2ee221ce5e4e5341803748
fathy <fathy@FATHYALI>
2025-12-10 03:46:32 +0200

    rename projects

---
+++ LiveAuction.Infrastructure/Presistence/EntitiesConfiguration/BidConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using LiveAuction.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LiveAuction.Infrastructure.Presistence.EntitiesConfiguration;
+
+internal class BidConfiguration : IEntityTypeConfiguration<Bid>
+{
+    public void Configure(EntityTypeBuilder<Bid> builder)
+    {
+        builder.HasKey(b => b.Id);
+        builder.Property(b => b.Amount)
+            .IsRequired()
+            .HasColumnType("decimal(18,2)");
+        builder.Property(b => b.BidTime)
+            .IsRequired();
+
+        builder.HasOne(b => b.Auction)
+            .WithMany(a => a.Bids)
+            .HasForeignKey(b => b.AuctionId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(b => b.Bidder)
+            .WithMany()
+            .HasForeignKey(b => b.BidderId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+    }
+}
