86180db9e4fafee7fd2677698ca875bfe66c506f
fathy <fathy@FATHYALI>
2025-12-12 23:45:52 +0200

    display name in home page

--- LiveAuction.Client/Auth/CustomAuthStateProvider.cs
+++ LiveAuction.Client/Auth/CustomAuthStateProvider.cs
@@ -42,7 +42,38 @@ public class CustomAuthStateProvider(
         var payload = jwt.Split('.')[1];
         var jsonBytes = ParseBase64WithoutPadding(payload);
         var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
-        return keyValuePairs!.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()!));
+
+        var claims = new List<Claim>();
+
+        foreach (var kvp in keyValuePairs)
+        {
+            if (kvp.Key == "role" || kvp.Key == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role")
+            {
+                if (kvp.Value.ToString().Trim().StartsWith("["))
+                {
+                    var parsedRoles = JsonSerializer.Deserialize<string[]>(kvp.Value.ToString() ?? string.Empty);
+                    foreach (var parsedRole in parsedRoles)
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, parsedRole));
+                    }
+                }
+                else
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, kvp.Value.ToString()??string.Empty));
+                }
+            }
+            else if (kvp.Key == "name")
+            {
+                claims.Add(new Claim(ClaimTypes.Name, kvp.Value.ToString()??string.Empty)); 
+                claims.Add(new Claim(kvp.Key, kvp.Value.ToString() ?? string.Empty)); 
+            }
+            else
+            {
+                claims.Add(new Claim(kvp.Key, kvp.Value.ToString() ?? string.Empty));
+            }
+        }
+
+        return claims;
     }
 
     private byte[] ParseBase64WithoutPadding(string base64)
