34e9b0b64b22e18e8640048293dbff9789a441a4
fathy <fathy@FATHYALI>
2025-12-21 10:15:24 +0200

    add watchlist

---
+++ LiveAuction.Infrastructure/Migrations/20251221081436_AddWatchList.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LiveAuction.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWatchList : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WatchLists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WatchLists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WatchLists_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WatchListItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WatchListId = table.Column<int>(type: "int", nullable: false),
+                    AuctionId = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CurrentBid = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    AuctionEndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WatchListItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WatchListItems_WatchLists_WatchListId",
+                        column: x => x.WatchListId,
+                        principalTable: "WatchLists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WatchListItems_WatchListId",
+                table: "WatchListItems",
+                column: "WatchListId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WatchLists_Id_UserId",
+                table: "WatchLists",
+                columns: new[] { "Id", "UserId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WatchLists_UserId",
+                table: "WatchLists",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WatchListItems");
+
+            migrationBuilder.DropTable(
+                name: "WatchLists");
+        }
+    }
+}
---
+++ LiveAuction.Infrastructure/Presistence/EntitiesConfiguration/WatchListConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using LiveAuction.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LiveAuction.Infrastructure.Presistence.EntitiesConfiguration;
+
+internal class WatchListConfiguration : IEntityTypeConfiguration<WatchList>
+{
+    public void Configure(EntityTypeBuilder<WatchList> builder)
+    {
+        builder.HasIndex(wl => new {wl.Id,wl.UserId}).IsUnique();
+
+        builder.HasMany(wl => wl.Items)
+            .WithOne(wli => wli.WatchList)
+            .HasForeignKey(wli => wli.WatchListId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(wl => wl.User)
+            .WithOne(u => u.WatchList)
+            .HasForeignKey<WatchList>(wl => wl.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+    }
+}
