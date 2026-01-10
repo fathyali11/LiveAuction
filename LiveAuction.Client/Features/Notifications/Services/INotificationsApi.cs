4532f9866d44ee6f5b7762dffd820ae03625740f
fathy <fathy@FATHYALI>
2026-01-10 11:38:39 +0200

    add INotificationsApi

---
+++ LiveAuction.Client/Features/Notifications/Services/INotificationsApi.cs
@@ -0,0 +1,19 @@
+﻿using LiveAuction.Shared.DTOs;
+using Refit;
+
+namespace LiveAuction.Client.Features.Notifications.Services;
+
+public interface INotificationsApi
+{
+    [Get("/api/notifications")]
+    Task<HttpResponseMessage> GetNotificationsAsync();
+
+    [Get("/api/notifications/count")]
+    Task<HttpResponseMessage> GetUnreadCountAsync();
+
+    [Post("/api/notifications/read/{id}")]
+    Task<HttpResponseMessage> MarkAsReadAsync(int id);
+
+    [Post("/api/notifications/read-all")]
+    Task<HttpResponseMessage> MarkAllAsReadAsync();
+}
