747fffc016a090ad1009233b4948933c6248b520
fathy <fathy@FATHYALI>
2026-01-21 01:44:28 +0200

    write error messages in arabic

--- LiveAuction.Application/ApplicationUsers/Commands/RegisterUser/RegisterUserCommandValidator.cs
+++ LiveAuction.Application/ApplicationUsers/Commands/RegisterUser/RegisterUserCommandValidator.cs
@@ -7,17 +7,17 @@ internal class RegisterUserCommandValidator:AbstractValidator<RegisterUserComman
     public RegisterUserCommandValidator()
     {
         RuleFor(x => x.FullName)
-            .NotEmpty().WithMessage("Full name is required.")
-            .MaximumLength(100).WithMessage("Full name must not exceed 100 characters.");
+              .NotEmpty().WithMessage("الاسم بالكامل مطلوب.")
+              .MaximumLength(100).WithMessage("يجب ألا يتجاوز الاسم 100 حرف.");
+
         RuleFor(x => x.Email)
-            .NotEmpty().WithMessage("Email is required.")
-            .EmailAddress().WithMessage("A valid email is required.");
+             .NotEmpty().WithMessage("البريد الإلكتروني مطلوب.")
+             .EmailAddress().WithMessage("يرجى إدخال بريد إلكتروني صالح.");
 
         RuleFor(x => x.Password)
-            .NotEmpty().WithMessage("Password is required.")
-            .Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)\S{8,}$")
-            .WithMessage("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.");
-
+             .NotEmpty().WithMessage("كلمة المرور مطلوبة.")
+             .Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)\S{8,}$")
+             .WithMessage("يجب أن تتكون كلمة المرور من 8 أحرف على الأقل، وتحتوي على حرف كبير، وحرف صغير، ورقم، ورمز خاص.");
 
     }
 }
