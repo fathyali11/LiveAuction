ac2b2b151b10b4d45c9a2adae6ef63df567110cd
fathy <fathy@FATHYALI>
2026-01-21 02:05:48 +0200

    fix url

--- LiveAuction.Client/Consts/HostUrl.cs
+++ LiveAuction.Client/Consts/HostUrl.cs
@@ -2,7 +2,7 @@
 
 public static class HostUrl
 {
-    public static readonly string Development = "https://localhost:7233";
+    public static readonly string Development = "https://localhost:7293";
     public static readonly string Production = "https://liveauction.runasp.net";
 
     public static string Current => Production;
